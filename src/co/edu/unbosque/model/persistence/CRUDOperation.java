bb1bb62faeadd1cff57ce4a6639440e2e29bf109
aulainformatica <aulainformatica@UBSCOB209-25.unbosque.edu.co>
2024-05-02 10:55:04 -0500

    Implementacion interface CRUD y ciclistaDAO

--- src/co/edu/unbosque/model/persistence/CRUDOperation.java
+++ src/co/edu/unbosque/model/persistence/CRUDOperation.java
@@ -1,5 +1,33 @@
+/**
+ * The CRUDOperation interface defines the basic CRUD (Create, Read, Update, Delete) operations for managing data. Create search operation by identification and name.
+ * @param <E> the type of data on which the CRUD operations will be performed
+ * 
+ * @author  
+ * @version  
+ */
+
 package co.edu.unbosque.model.persistence;
 
-public interface CRUDOperation {
+import java.util.ArrayList;
+
+public interface CRUDOperation<E> {
+
+	/**
+	 * Creates a new entry for the provided data.
+	 * 
+	 * @param data the data to be created
+	 */
+
+	public void crear(E data);
+
+	public String actualizarPorIdentificador(int id, E newData);
+
+	public ArrayList<E> buscarNombre(String n);
+
+	public E buscarIdentificador(int id);
+
+	public String eliminarPorIdentificador(int id);
+
+	public ArrayList<E> mostrarTodos();
 
 }
--- src/co/edu/unbosque/model/persistence/CiclistaDAO.java
+++ src/co/edu/unbosque/model/persistence/CiclistaDAO.java
@@ -1,5 +1,109 @@
 package co.edu.unbosque.model.persistence;
 
-public class CiclistaDAO {
+import java.util.ArrayList;
+
+import co.edu.unbosque.model.CiclistaDTO;
+
+public class CiclistaDAO implements CRUDOperation<CiclistaDTO> {
+
+	private ArrayList<CiclistaDTO> listaCiclistas;
+	private final String SERIALIZED_FILE_NAME = "datos/Ciclista.isuci";
+
+	public CiclistaDAO() {
+		listaCiclistas = new ArrayList<>();
+		leerArchivoSerializado();
+	}
+
+	public void leerArchivoSerializado() {
+		Object contenido = FileHandler.abrirLeerSerializado(SERIALIZED_FILE_NAME);
+
+		if (contenido == null) {
+			listaCiclistas = new ArrayList<>();
+		} else {
+			listaCiclistas = (ArrayList<CiclistaDTO>) contenido;
+		}
+
+	}
+
+	public String checkIndex(int index) {
+		if (index < 0) {
+			return "La posición no puede tomar valores negativos";
+		}
+		if (index >= listaCiclistas.size()) {
+			return "La posición no puede ser mayor al tamaño de la lista, tamaño actual: " + listaCiclistas.size()
+					+ " datos";
+		}
+		return "g";
+	}
+
+	@Override
+	public void crear(CiclistaDTO data) {
+		listaCiclistas.add(data);
+		FileHandler.abrirEscribirSerializable(SERIALIZED_FILE_NAME, listaCiclistas);
+
+	}
+
+	@Override
+	public String actualizarPorIdentificador(int id, CiclistaDTO newData) {
+		String msj = "No existe un Ciclista con ese identificador";
+
+		for (int i = 0; i < listaCiclistas.size(); i++) {
+			if (listaCiclistas.get(i).getIdentificador() == id) {
+				listaCiclistas.set(i, newData);
+				FileHandler.abrirEscribirSerializable(SERIALIZED_FILE_NAME, listaCiclistas);
+				msj = "El Ciclista se ha actualizado con exito";
+				break;
+			}
+		}
+
+		return msj;
+	}
+
+	@Override
+	public ArrayList<CiclistaDTO> buscarNombre(String n) {
+
+		ArrayList<CiclistaDTO> ciclistas = null;
+
+		for (int i = 0; i < listaCiclistas.size(); i++) {
+			if (listaCiclistas.get(i).getNombre().toLowerCase().equals(n.toLowerCase())) {
+				ciclistas.add(listaCiclistas.get(i));
+			}
+		}
+		return ciclistas;
+	}
+
+	@Override
+	public CiclistaDTO buscarIdentificador(int id) {
+
+		for (int i = 0; i < listaCiclistas.size(); i++) {
+			if (listaCiclistas.get(i).getIdentificador() == id) {
+				return listaCiclistas.get(i);
+
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public String eliminarPorIdentificador(int id) {
+		String msj = "No existe un Ciclista con ese identificador";
+
+		for (int i = 0; i < listaCiclistas.size(); i++) {
+			if (listaCiclistas.get(i).getIdentificador() == id) {
+				listaCiclistas.remove(i);
+				FileHandler.abrirEscribirSerializable(SERIALIZED_FILE_NAME, listaCiclistas);
+				msj = "El Ciclista se ha eliminado con exito";
+				break;
+			}
+		}
+
+		return msj;
+	}
+
+	@Override
+	public ArrayList<CiclistaDTO> mostrarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
